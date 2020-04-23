18250d4b6f4634ada1934f98befc5714f91cda75
Christian <ctb77@stanford.edu>
2020-04-23 10:23:53 -0700

    Implement FAVE-style tsv output
    FAVE-align uses a simple and well documented tsv input format
    to which these transcripts often need converted for force
    alignment. This patch allows parsedtranscript files to be output
    in that FAVE tsv style.

--- TranscriptParser.py
+++ TranscriptParser.py
@@ -24,8 +24,7 @@ class TranscriptFile():
 	annotations = re.compile(r"{.*?}")
 	doubleParens = re.compile(r"\(\(.*?\)\)")
 	incompleteWords = re.compile(r"\b(\w*?)-\s+\+(.+?)\b")
-	def __init__(self,fname, type):
-		global inDir
+	def __init__(self,fname, type, inDir='.', outDir='.'):
 		self.type = type
 		self.fname = fname
 		tree = ET.parse(os.path.join(inDir,fname))
@@ -34,6 +33,8 @@ class TranscriptFile():
 		self.posTaggedArray = []
 		self.nlpParserInit = False
 		self.pos_tagger = None
+		self.data = []
+		self.outDir = outDir
 
 	def getTranscript(self):
 		return(self.textArray)
@@ -47,14 +48,26 @@ class TranscriptFile():
 			self.pos_tagger = CoreNLPParser(url=nlpURL, tagtype='pos')
 			self.nlpParserInit = True
 
-	def tag(self,line):
+	def tag(self):
+		data = self.data
+		if data == []:
+			raise ValueError('Transcript data is empty. Have you parsed it?')
 		if not self.nlpParserInit:
 			self.initNlpParser()
-		pos_tagged = list(self.pos_tagger.tag(line.split()))
-		return(pos_tagged)
+
+		fullText = []
+		posTaggedArray = []
+		for struct in data:
+			line = struct['text'].strip()
+			text = self._clean_line(line)
+			if text.strip() == '':
+				continue
+			tagged = self.pos_tagger.tag(text.split())
+			posTaggedArray.append(list(tagged))
+		self.posTaggedArray = posTaggedArray
 
 	def save(self,fname=None, type="transcript"):
-		global outDir
+		outDir = self.outDir
 		if type == "pos tagged":
 			ext = '.tsv'
 			tagged = self.posTaggedArray
@@ -95,125 +108,199 @@ class TranscriptFile():
 
 
 class ElanTranscriptionFile(TranscriptFile):
-	def __init__(self,fname):
-		TranscriptFile.__init__(self,fname,'eaf')
-		self.parse_eaf()
+	def __init__(self,fname,inDir='.',outDir='.'):
+		TranscriptFile.__init__(self,fname,'eaf',inDir,outDir)
+		self.timeOrders = []
 
-	def parse_eaf(self):
-		counter = 0
-		potentialSpeakerTiers = []
+	def _parseTimeOrders(self):
+		timeOrderTag = None
+		orders = []
+		for child in self.root:
+			if child.tag == 'TIME_ORDER':
+				for slot in child:
+					try:
+						orders.append(slot.attrib['TIME_VALUE'])
+					except KeyError:
+						print('Potential error in',self.fname)
+						orders.append(0)
+				break
+		self.timeOrders = orders
+
+	def _tsIndex(self, slot):
+		return(int(slot.split('s')[1])-1)
+
+	def parse(self):
+		data = []
+		self._parseTimeOrders()
+		timeOrders = self.timeOrders
 		for child in self.root:
 			if child.tag == 'TIER':
-				tierID = child.attrib['TIER_ID'].lower()
-				if 'interviewer' in tierID or 'unknown' in tierID:
-					continue
-				else:
-					potentialSpeakerTiers.append(child)
-					counter += 1
-		if counter == 0:
-			raise ValueError('Cannot identify speaker tier')
-		elif counter > 1:
-			speakerTier = self._eafIdentifySpeaker(potentialSpeakerTiers)
-		else:
-			speakerTier = potentialSpeakerTiers[0]
-		self._eafExtractTranscript(speakerTier)
+				name = child.attrib['TIER_ID']
+				for element in child.iter('ALIGNABLE_ANNOTATION'):
+					startRef = self._tsIndex(element.attrib['TIME_SLOT_REF1'])
+					endRef = self._tsIndex(element.attrib['TIME_SLOT_REF2'])
+					start = int(timeOrders[startRef])/1000.0  # convert ms to seconds
+					end = int(timeOrders[endRef])/1000.0  # convert ms to seconds
+					text = element.find('ANNOTATION_VALUE').text
+					if text == None:
+						continue
+					text = self._clean_line(text)
+					if text.strip() == '':
+						continue
+					line = {
+						'speaker': name,
+						'start': start,
+						'end': end,
+						'text': text
+					}
+					data.append(line)
+		self.data = sorted(data,key=lambda x:float(x['start']))
 
-	def _eafIdentifySpeaker(self,potentialSpeakerTiers):
-		try:
-			tiersNamedSpeaker = [x for x in potentialSpeakerTiers if 'speaker' in x.attrib['TIER_ID'].lower()]
-		except Exception as e:
-			print(potentialSpeakerTiers)
-			exit(e)
-		if len(tiersNamedSpeaker) == 1:
-			return(tiersNamedSpeaker[0])
-		elif len(tiersNamedSpeaker) == ( len( potentialSpeakerTiers ) - 1 ):
-			return( [x for x in potentialSpeakerTiers if 'speaker' not in x.attrib['TIER_ID'].lower][0] )
+	def text(self,speakerIDs=None):
+		if speakerIDs:
+			if type(speakerIDs) == list:
+				raise NotImplementedError() # Need to move code determination to self.parse() from FaveTsv
+			elif type(speakerIDs) == str:
+				raise NotImplementedError()
+			else:
+				raise TypeError('IDs must be a string or list of strings')
 		else:
-			raise ValueError('Cannot identify speaker tier')
+			return('\n'.join([x['text'] for x in self.data]))
 
-	def _eafExtractTranscript( self, speakerTier ):
-		fullText = []
-		posTaggedArray = []
-		for text in speakerTier.itertext():
-			text = self._clean_line(text)
-			if text.strip() == '':
-				continue
-			fullText.append(text)
-			try:
-				posTaggedArray.append(self.tag(text))
-			except Exception as e:
-				print(repr(text))
-				exit(e)
-		self.textArray = fullText
-		self.posTaggedArray = posTaggedArray
 
 class TranscriberTranscriptFile(TranscriptFile):
-	def __init__(self,fname):
-		TranscriptFile.__init__(self,fname,'trs')
+	def __init__(self,fname,inDir='.',outDir='.'):
+		TranscriptFile.__init__(self,fname,'trs',inDir,outDir)
 		self.speakerID = None
-		self.parse_transcriber()
+		self.speakers = {
+			'Unidentified Speaker':'Unidentified Speaker'
+		}
 
-
-	def parse_transcriber(self):
-		counter = 0
-		potentialSpeakers = []
+	def parse(self):
+		speakers = self.speakers
+		data = []
 		for child in self.root:
 			if child.tag == "Speakers":
 				for speaker in child:
-					name = speaker.attrib['name'].lower()
-					if (
-						'interviewer' in name or
-						'other' in name or
-						'unknown' in name or
-						'unidentified' in name or
-						'stranger' in name or
-						'\\' in name
-					):
-						continue
-					potentialSpeakers.append(speaker.attrib)
-					counter += 1
-				if counter == 0:
-					raise ValueError('Cannot identify speaker ID')
-				elif counter > 1:
-					speakerID = self._trsIdentifySpeaker(potentialSpeakers)
-				else:
-					speakerID = potentialSpeakers[0]['id']
-				self.speakerID = speakerID
-			if child.tag == "Episode":
-				self._trsExtractTranscript(child)
+					name = speaker.attrib['name']
+					id_ = speaker.attrib['id']
+					speakers[id_] = name
+			elif child.tag == 'Episode':
+				for turn in child.iter('Turn'):
+					lines = self._parseTurn(turn)
+					data = data + lines
+		self.data = data
 
-	def _trsIdentifySpeaker(self,potentialSpeakers):
-		for speaker in potentialSpeakers:
-			name = speaker['name'].split(' ')[::-1]
-			if '_'.join(name) in self.fname:
-				print('!!ACCEPT!!')
-				return(speaker['id'])
-		raise ValueError('Cannot identify speaker ID')
+	def _parseTurn(self, turn):
+		try:
+			speakerList = turn.attrib['speaker'].split()
+		except KeyError:
+			# print('WARN: Turn has no speaker')
+			speakerList = [None]
+		data = []
+		if len(speakerList) == 1:
+			data = data + self._parseSingleSpeakerTurn(turn)
+		elif len(speakerList) > 1:
+			data = data + self._parseMultiSpeakerTurn(turn)
+		else:
+			raise ValueError('Turn has no speaker')
+		return(data)
 
-	def _trsExtractTranscript(self,contentTag):
-		speakerID = self.speakerID
-		fullText = []
-		posTaggedArray = []
-		for turn in contentTag.iter('Turn'):
+	def _parseSingleSpeakerTurn(self, turn):
+		cache = []
+		turnEnd = turn.attrib['endTime']
+		syncIter = turn.iter()
+		next(syncIter)
+		for subelement in syncIter:
+			tail = subelement.tail.strip()
+			start = float(subelement.attrib['time'])
 			try:
-				attribID = turn.attrib['id']
+				speakerID = turn.attrib['speaker']
 			except KeyError:
-				try:
-					attribID = turn.attrib['speaker']
-				except KeyError:
-					print('Turn with no ID')
-					print(turn.attrib)
-					continue
-			if attribID != speakerID:
-				continue
-			for text in turn.itertext():
-				text = self._clean_line(text)
-				if text.strip() == '':
-					continue
-				fullText.append(text)
-				posTaggedArray.append(self.tag(text))
-		self.textArray = fullText
-		self.posTaggedArray = posTaggedArray
+				speakerID = 'Unidentified Speaker'
+			struct = {
+				'speaker': self.speakers[speakerID],
+				'start': start,
+				'end': None,
+				'text': tail
+			}
+			cache.append(struct)
+		cache[-1]['end'] = float(turnEnd)
+		for i in range(len(cache)-1):
+			cache[i]['end'] = float(cache[i+1]['start'])
+		return(cache)
+
+	def _parseMultiSpeakerTurn(self, turn):
+		cache = {}
+		out = []
+		syncPoint = 0.0
+		turnEnd = float(turn.attrib['endTime'])
+		for i in range(len(list(turn))):
+			subelement = turn[i]
+			tail = subelement.tail.strip()
+			if subelement.tag == 'Sync':
+				syncPoint = subelement.attrib['time']
+			elif subelement.tag == 'Who':
+				code = 'spk'+subelement.attrib['nb']
+				struct = {
+					'speaker': self.speakers[code],
+					'start': float(syncPoint),
+					'end': turnEnd if i == len(list(turn)) else None,
+					'text': tail
+				}
+				if code not in cache:
+					cache[code] = []
+				cache[code].append(struct)
+		for k,v in cache.items():
+			for i in range(len(v)-1):
+				v[i]['end'] = v[i+1]['start']
+			out = out + v
+		return(out)
+
+
+class FaveTsv():
+	def __init__(self):
+		self.output = ''
+
+	def convert(
+		self,
+		transcript
+	):
+		codes = {}
+		codeCounter = 0
+		try:
+			data = sorted(transcript.data,key=lambda x:float(x['start']))
+		except TypeError:
+			print(transcript.data)
+			exit()
+		outArray = []
+		for row in data:
+			speaker = row['speaker']
+			if speaker not in codes:
+				codes[speaker] = str(codeCounter)
+				codeCounter += 1
+			lineArray = [
+				str(codes[speaker]),
+				str(speaker),
+				str(row['start']),
+				str(row['end']),
+				str(row['text'])
+			]
+			line = '\t'.join(lineArray)
+			outArray.append(line)
+		out = '\n'.join(outArray)
+		self.output = out
+
+	def save(self,fname):
+		global outDir
+		fname = fname.split('.')[0]+'.txt'
+		outPath = os.path.join(outDir,fname)
+		with open(outPath,'w') as f:
+			f.write(self.output)
+
+	def clear(self):
+		self.output=''
+
 
 def main(limit=0):
 	global inDir
@@ -221,23 +308,34 @@ def main(limit=0):
 	global listPath
 	if listPath:
 		with open(listPath) as f:
-			transcriptFiles = f.readlines()
+			transcriptFiles = [x.strip() for x in f.readlines()]
 	else:
 		transcriptFiles = os.listdir(inDir)
 	limitCounter = 0
+	fave = FaveTsv()
 	for fname in transcriptFiles:
+		if not os.path.isfile(os.path.join(inDir,fname)):
+			print(f'Cannot find file: {fname}')
+			continue
 		print(fname)
 		try:
 			if '.eaf' in fname:
-				transcript = ElanTranscriptionFile(fname)
+				transcript = ElanTranscriptionFile(fname,inDir,outDir)
 			elif '.trs' in fname:
-				transcript = TranscriberTranscriptFile(fname)
+				transcript = TranscriberTranscriptFile(fname,inDir,outDir)
 			else:
 				continue
 		except ValueError as e:
 			print(f'Could not parse {fname}')
 			continue
-		transcript.save(type=outType)
+		transcript.parse()
+		if outType == 'pos tagged':
+			transcript.tag()
+			transcript.save(type=outType)
+		else:
+			fave.convert(transcript)
+			fave.save(fname)
+			fave.clear()
 		del transcript
 		limitCounter += 1
 		if limit > 0 and limitCounter >= limit:
@@ -265,13 +363,11 @@ if __name__ == "__main__":
 		default=0
 	)
 	argparser.add_argument(
-		"-g",
 		"--tag",
 		help="Run the part of speech tagger",
 		action="store_true"
 	)
 	argparser.add_argument(
-		"-x",
 		"--text",
 		help="Plain text output",
 		action="store_true"
@@ -290,7 +386,6 @@ if __name__ == "__main__":
 		default="9000"
 	)
 	argparser.add_argument(
-		"-l",
 		"--list",
 		help="Path to a file containing a list of items to parse."
 	)
